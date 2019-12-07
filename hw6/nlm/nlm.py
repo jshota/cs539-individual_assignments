5d006ddfef3c961e30efaee6e290a3b4ce58a1a1
Jing <jshota@hotmail.com>
2019-12-06 22:45:45 -0800

    New version

--- hw6/nlm/nlm.py
+++ hw6/nlm/nlm.py
@@ -25,31 +25,38 @@ class NLM:
         with open('./saved_models/{}/model.pt'.format(model_name), 'rb') as rf:
             NLM.model.load_state_dict(torch.load(rf, map_location='cpu'))
 
-    def __init__(self, hidden=None, history=[]):
+    def __init__(self, xs=None, hidden=None, history=[]): # can supply init string
         if hidden is None:
             self.hidden = NLM.model.init_hidden(1, NLM.args.layer_num)
             self.history = []
             self += '<s>'
+            if xs is not None: # init string
+                self.__iadd__(xs)
         else:
-            self.hidden=hidden
+            self.hidden = hidden
             self.history = history
+        self.__repr__ = self.__str__
     
-    def __add__(self, x):
-        x_id = NLM.vocab[x]
-        input_ids = torch.Tensor([[x_id]]).long().to(NLM.args.device)
-        hidden = NLM.model(input_ids, self.hidden)
-        return NLM(hidden, self.history + [x])
+    def __add__(self, xs):
+        hidden, history = self.hidden, self.history[:] # copy!
+        for x in xs.split():
+            x_id = NLM.vocab[x]
+            input_ids = torch.Tensor([[x_id]]).long().to(NLM.args.device)
+            hidden = NLM.model(input_ids, hidden)
+            history.append(x)
+        return NLM(hidden=hidden, history=history)
 
-    def __iadd__(self, x):
-        x_id = NLM.vocab[x]
-        input_ids = torch.Tensor([[x_id]]).long().to(NLM.args.device)
-        self.hidden = NLM.model(input_ids, self.hidden)
-        self.history.append(x)
+    def __iadd__(self, xs):
+        for x in xs.split():
+            x_id = NLM.vocab[x]
+            input_ids = torch.Tensor([[x_id]]).long().to(NLM.args.device)
+            self.hidden = NLM.model(input_ids, self.hidden)
+            self.history.append(x)
         return self
     
     def next_prob(self, char=None):
         output = NLM.model.fc(self.hidden[0])
-        output = output[:,-1,:].squeeze()
+        output = output[-1,-1,:].squeeze() # two layers
         probs = F.softmax(output[NLM.OUTPUTLISTIDX], dim=-1).tolist()
         if char == None:
             return {ch:prob for ch, prob in zip(NLM.OUTPUTLIST, probs)}
@@ -62,7 +69,9 @@ class NLM:
         
     ___repr__ = __str__
     
-def evaluate_a_string():
+if __name__ == "__main__":
+    NLM.load('huge')
+    '''
     # evaluate a string
     h = NLM()
     p = 1
@@ -71,31 +80,34 @@ def evaluate_a_string():
         p *= h.next_prob(c)
         h += c
 
-def greedy_generation():
     # greedy generation
-    h = NLM()
-    for i in range(100):
+    h = NLM("d o n a l d _ t r u m p ")
+    for _ in range(100):
         c, p = max(h.next_prob().items(), key=lambda x: x[1])
         print(c, "%.2f <- p(%s | ... %s)" % (p, c, " ".join(map(str, h.history[-4:]))))
         h += c
 
-def shannon_game():
-    # shannon game
-    text = "b  r  o  w  n  _  b  e  a  r  _  w  a  s  _  a  l  l  o  w  e  d  _  i  n  t  o  _  t  h  e  _  c  i  r  c  u  s  _  t  e  n  t  _  w  i  t  h  o  u  t  _  p  a  y  i  n  g  _  b  e  c  a  u  s  e  _ ".split()
-    h = NLM()
-    guesses = []
-    for c in text:
-        order = sorted(h.next_prob().items(), key=lambda x: -x[1])
-        for i, (char, prob) in enumerate(order):
-            if c == char:
+    # random generation
+    h = NLM("h i l l a r y _ c l i n t o n")
+    t = 0.5
+    import random
+    for _ in range(10):
+        h = NLM("h i l l a r y _ c l i n t o n")
+        chars = list(h.next_prob().keys())
+        while c != "</s>":
+            probs = h.next_prob()
+            #s = sum(p ** (1/t) for p in probs.values())
+            probs = {c: p ** (1/t) for (c, p) in probs.items()}
+            [choice] = random.choices(chars, [probs[c] for c in chars])
+            print(choice, end=' ')
+            h += choice
+            if choice == "</s>":
+                print()
+                h = NLM()
                 break
-        guesses.append(i+1)
-        h += c
-    print(" ".join("%2s" % c for c in text))
-    print(" ".join("%2d" % g for g in guesses))
-
-def entropy():
-    #entropy
+        print()
+    '''
+    # entropy    
     p = 0
     l = 0
     import math, sys
@@ -109,7 +121,4 @@ def entropy():
         l += len(line) + 1
     print(p / l)
 
-if __name__ == "__main__":
-    NLM.load('large')
-
-    entropy()
+    
